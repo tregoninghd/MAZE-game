5e4cc9c1ac48134dc140b97ac827a43bc1c4de30
tregoninghd <tregoninghd@outlook.com>
2025-12-18 12:59:51 -0500

    Update maze-create.py

--- maze-create.py
+++ maze-create.py
@@ -8,9 +8,24 @@ maze = [
     ["#", "#", "#", "#", "#", "#", "#"]
 ]
 
+player_pos = (1,1)
+maze[player_pos[0]][player_pos[1]] = "P"
+
 def draw_maze(maze):
     for row in maze:
         print("".join(row))
+
+
+def move_player(maze, player_pos, new_row, new_col):
+    row,col = player_pos
+
+    if maze [new_row][new_col] != "#":
+        maze[row][col] = " "
+        maze[new_row][new_col] = "P"
+        return (new_row, new_col)
+    
+    return player_pos
+
 
 def clear_console():
     os.system("cls")
