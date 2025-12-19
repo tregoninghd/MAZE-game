cec1c491a3ab72ee2d1850bf4a120429bef0ad90
tregoninghd <tregoninghd@outlook.com>
2025-12-19 18:00:14 -0500

    Update maze-create.py

--- maze-create.py
+++ maze-create.py
@@ -6,14 +6,16 @@ WALL = "wall"
 FLOOR = "floor"
 PLAYER = "player"
 EXIT = "exit"
-
+KEY = "key"
+DOOR = "door"
 
 TILE_RENDER = {
     WALL : "█",   # pared
     FLOOR : " ",   # camino
     PLAYER : "H",   # jugador
-    EXIT : "X"    # salida
-    
+    EXIT : "X",   # salida
+    KEY : "K", # llave
+    DOOR : "D", # puerta
 }
 
 def create_empty_maze(rows, cols):  # Función para crear un laberinto vacío
@@ -24,9 +26,6 @@ def create_empty_maze(rows, cols):  # Función para crear un laberinto vacío
             row.append({"type": WALL})
         maze.append(row)
     return maze
-
-
-
 
 def generate_maze_dfs(maze, row, col):
     maze[row][col]["type"] = FLOOR
@@ -46,7 +45,6 @@ def generate_maze_dfs(maze, row, col):
             maze[new_row][new_col]["type"] = FLOOR # Marcar la nueva celda como camino
             generate_maze_dfs(maze, new_row, new_col) # Llamada recursiva para continuar generando el laberinto
 
-
 def draw_maze(maze): # Función para dibujar el laberinto
     for row in maze:
         rendered_row = ""
@@ -57,9 +55,18 @@ def draw_maze(maze): # Función para dibujar el laberinto
             rendered_row += TILE_RENDER.get(cell_type, "?")# Renderizar cada celda del laberinto
         print(rendered_row)
 
-
-def move_player(maze, player_pos, new_row, new_col): # Función para mover al jugador
+def move_player(maze, player_pos, new_row, new_col, inventory): # Función para mover al jugador
     row,col = player_pos
+    target_type = maze[new_row][new_col]["type"]
+    if target_type == KEY:                          ##### Que pasa si hay una llave en frente
+        inventory["keys"] += 1
+        maze[new_row][new_col]["type"] = FLOOR
+    if target_type == DOOR:                         ##### QUe pasa si hay una puerta en frente
+        if inventory["keys"] > 0:
+            inventory["keys"] -= 1
+            maze[new_row][new_col]["type"] = FLOOR
+        else:
+            return player_pos 
     if maze [new_row][new_col]["type"] in (FLOOR, EXIT):
         maze[row][col]["type"] = FLOOR # Limpiar la posición anterior del jugador
         maze[new_row][new_col]["type"] = PLAYER # Mover al jugador a la nueva posición
@@ -85,15 +92,40 @@ def calculate_new_position(player_pos, move):
         return row, col + 1
     return row,col
 
+def place_key(maze):
+    rows = len(maze)
+    cols = len(maze[0])
+
+    while True:
+        r = random.randrange(1,rows -1)
+        c = random.randrange(1,cols -1)
+
+        if maze[r][c]["type"] == FLOOR:
+            maze[r][c]["type"] = KEY
+            return (r,c)
+        
+def place_door(maze, exit_pos):
+    r, c = exit_pos
+    door_pos = (r -1, c) # Hacer aleatoreo la distancia entre la puerta y la salida
+
+    if maze[door_pos[0]][door_pos[1]]["type"] == FLOOR:
+        maze[door_pos[0]][door_pos[1]]["type"] = DOOR
+        return door_pos
+    
+    return None
+
 def play_round(rows, cols):
     maze = create_empty_maze(rows,cols)
+    inventory = {
+        "keys": 0
+    }
 
     # Marca la celda inicial como camino
     maze[1][1]["type"] = FLOOR
     generate_maze_dfs(maze, 1,1)
 
     exit_pos = (len(maze)-2, len(maze[0])-2)
-
+   
     # Aquí se asegura que la salida esté en un camino
     while maze[exit_pos[0]][exit_pos[1]] == WALL:
         maze = create_empty_maze(rows,cols)
@@ -106,6 +138,9 @@ def play_round(rows, cols):
     exit_pos = (len(maze)-2, len(maze[0])-2) # Posición de la salida
     maze[exit_pos[0]][exit_pos[1]]["type"] = EXIT
 
+    key_pos = place_key(maze)
+    door_pos = place_door(maze, exit_pos)
+
     while True:
         clear_console()
         draw_maze(maze)
@@ -115,7 +150,7 @@ def play_round(rows, cols):
             return False # Salir del juego
 
         new_row, new_col = calculate_new_position(player_pos, move) # Calcular la nueva posición del jugador
-        player_pos = move_player(maze, player_pos, new_row, new_col) # Mover al jugador
+        player_pos = move_player(maze, player_pos, new_row, new_col, inventory) # Mover al jugador
 
         if player_pos == exit_pos:
             draw_maze(maze)
@@ -127,8 +162,8 @@ def play_round(rows, cols):
 
 round_number = 1  #Luego debo de anotar incrementos por ronda
 
-rows = 21
-cols = 51
+rows = 11
+cols = 25
 
 while True:
     won = play_round(rows, cols)
